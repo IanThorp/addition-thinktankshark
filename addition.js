19d51caa9fa0aa8caa4b293d658b700e92e195c9
Ian Thorp <IanMThorp@gmail.com>
2016-04-16 12:27:36 -0700

    Fix all the code

--- addition.js
+++ addition.js
@@ -1,3 +1,6 @@
 module.exports = function (a, b) {
 	return a + b
 }
+
+console.log("Sam is going to lose in the cornhole tournament.")
+console.log("Is this seriously the quality of code he writes? It sucks. ")
